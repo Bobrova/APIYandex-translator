d3ae816c6a4b30035d4d886c27bcb8c259aacbbb
Bobrova <nastyabobr@yandex.ru>
2018-11-23 20:06:41 +0300

    commit

--- YandexTranslator.js
+++ YandexTranslator.js
@@ -1,14 +1,14 @@
 (function(){
-    "use strict";
+  "use strict";
 
   function textTranslate(text, lang,  apiKey, r_handler){
     var text = encodeURIComponent(text);
     var type = "application/x-www-form-urlencoded";
     
     var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?" 
-          + "key=" + apiKey
-          + "&text=" + text
-          + "&lang=" + lang;
+            + "key=" + apiKey
+            + "&text=" + text
+            + "&lang=" + lang;
 
     var myInit = { method: 'POST',
                    body: text,
@@ -16,55 +16,53 @@
 
     fetch(url, myInit)
     .then(function(response) {
-    response.text().then(function(text) {
-      var data = JSON.parse(text);
-      r_handler(data.text[0]);
+      response.text().then(function(text) {
+        var data = JSON.parse(text);
+        r_handler(data.text[0]);
+      });
     });
-  });
- 
-}
+  }
 
-function identifyLanguage (text, apiKey,r_handler){
-  var text = encodeURIComponent(text);
-  var type = "application/x-www-form-urlencoded";
+  function identifyLanguage (text, apiKey,r_handler){
+    var text = encodeURIComponent(text);
+    var type = "application/x-www-form-urlencoded";
     
-  var url = "https://translate.yandex.net/api/v1.5/tr.json/detect?" 
-          + "key=" + apiKey
-          + "&text=" + text;
+    var url = "https://translate.yandex.net/api/v1.5/tr.json/detect?" 
+            + "key=" + apiKey
+            + "&text=" + text;
 
-  var myInit = { method: 'POST',
-                 body: text,
-                 headers: {"Content-Type": type}};
+    var myInit = { method: 'POST',
+                   body: text,
+                   headers: {"Content-Type": type}};
 
-  fetch(url, myInit)
-  .then(function(response) {
-    response.text().then(function(text) {
+    fetch(url, myInit)
+    .then(function(response) {
+      response.text().then(function(text) {
       var data = JSON.parse(text);
       r_handler(data.lang);
+      });
     });
-  });
-}
+  }
 
-function getLanguages(ui, apiKey, r_handler){
-  var type = "application/x-www-form-urlencoded";
+  function getLanguages(ui, apiKey, r_handler){
+    var type = "application/x-www-form-urlencoded";
     
-  var url = "https://translate.yandex.net/api/v1.5/tr.json/getLangs?" 
-          + "key=" + apiKey;
+    var url = "https://translate.yandex.net/api/v1.5/tr.json/getLangs?" 
+            + "key=" + apiKey;
 
-  var myInit = { method: 'POST',
+    var myInit = { method: 'POST',
                  headers: {"Content-Type": type}};
 
-  fetch(url, myInit)
-  .then(function(response) {
-    response.text().then(function(text) {
+    fetch(url, myInit)
+    .then(function(response) {
+      response.text().then(function(text) {
       var data = JSON.parse(text);
       r_handler(data.dirs);
+      });
     });
-  });
-}
+  }
 
-
- // commonjs
+  // commonjs
   if (typeof exports === "object") {
     module.exports = textTranslate;
     module.exports = identifyLanguage;

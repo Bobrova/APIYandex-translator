e8f73e08594a2dec1ea06a904e09e03f08f988a4
Nastya Bobrova <deepkilt89@gmail.com>
2018-12-07 21:32:53 +0300

    request

--- src/YandexTranslator.js
+++ src/YandexTranslator.js
@@ -1,64 +1,69 @@
-  export function textTranslate(text, lang,  apiKey, r_handler){
-    var text = encodeURIComponent(text);
-    var type = "application/x-www-form-urlencoded";
-    
-    var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?" 
-            + "key=" + apiKey
-            + "&text=" + text
-            + "&lang=" + lang;
+export function textTranslate(text, lang,  apiKey, r_handler){
+  var text = encodeURIComponent(text);
+  var type = 'application/x-www-form-urlencoded';
+  
+  var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?' 
+    + 'key=' + apiKey
+    + '&text=' + text
+    + '&lang=' + lang;
 
-    var myInit = { method: 'POST',
-                   body: text,
-                   headers: {"Content-Type": type}};
+  var myInit = { 
+    method: 'POST',
+    body: text,
+    headers: {'Content-Type': type}};
 
+  request(url, myInit, 'translate', r_handler);
+}
+
+export function detect (text, apiKey, r_handler){
+  var text = encodeURIComponent(text);
+  var type = 'application/x-www-form-urlencoded';
+  
+  var url = 'https://translate.yandex.net/api/v1.5/tr.json/detect?' 
+    + 'key=' + apiKey
+    + '&text=' + text;
+
+  var myInit = { 
+    method: 'POST',
+    body: text,
+    headers: {'Content-Type': type}
+  };
+
+  request(url, myInit, 'detect', r_handler);
+}
+
+export function getLanguages(id, ui, apiKey, r_handler){
+  var type = 'application/x-www-form-urlencoded';
+  
+  var url = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs?' 
+    + 'key=' + apiKey;
+
+  var myInit = { 
+    method: 'POST',
+    headers: {'Content-Type': type}};
+
+  request(url, myInit, 'getLanguages', r_handler);
+}
+
+
+function request(url, myInit, method, r_handler){
     fetch(url, myInit)
-    .then(function(response) {
-      response.text().then(function(text) {
-        var data = JSON.parse(text);
-        //console.log(data.text[0]);
-        r_handler(data.text[0]);
+      .then(function(response) {
+        response.text().then(function(text) {
+          var data = JSON.parse(text);
+            switch (method){
+              case 'translate':
+                r_handler(data.text[0]);
+                break;
+              case 'detect':
+                r_handler(data.lang);
+                break;
+              case 'getLanguages':
+                r_handler(data.dirs);
+                break;
+            }
+          });
       });
-    });
   }
-
- export function identifyLanguage (text, apiKey, r_handler){
-    var text = encodeURIComponent(text);
-    var type = "application/x-www-form-urlencoded";
-    
-    var url = "https://translate.yandex.net/api/v1.5/tr.json/detect?" 
-            + "key=" + apiKey
-            + "&text=" + text;
-
-    var myInit = { method: 'POST',
-                   body: text,
-                   headers: {"Content-Type": type}};
-
-    fetch(url, myInit)
-    .then(function(response) {
-      response.text().then(function(text) {
-      var data = JSON.parse(text);
-      r_handler(data.lang);
-      });
-    });
-  }
-
- export function getLanguages(id, ui, apiKey, r_handler){
-    var type = "application/x-www-form-urlencoded";
-    
-    var url = "https://translate.yandex.net/api/v1.5/tr.json/getLangs?" 
-            + "key=" + apiKey;
-
-    var myInit = { method: 'POST',
-                 headers: {"Content-Type": type}};
-
-    fetch(url, myInit)
-    .then(function(response) {
-      response.text().then(function(text) {
-      var data = JSON.parse(text);
-      r_handler(data.dirs);
-      });
-    });
-  }
-
 
 
